e0a156d9c8725ab604a4a4403f3e37856cf9f99f
Aman Kumar Gupta <aman65144kumar@gmail.com>
2023-06-20 20:26:14 +0530

    Added _01_hello.java and _02_variables_and_IO.java

---
+++ _01_hello.java
@@ -0,0 +1,10 @@
+public class _01_hello {
+    public static void main(String[] args) {
+        System.out.print("Hello, Spiderman!");
+    }
+}
+/*
+ * OUTPUT
+ * 
+ * Hello, Spiderman!
+ */
---
+++ _02_variables_and_IO.java
@@ -0,0 +1,47 @@
+public class _02_variables_and_IO {
+
+    public static void main(String[] args) {
+        // Primitive Data Types
+        int a = 78;
+        float b = 32.22f;
+        double c = 232.22343d;
+        char d = 'f';
+        long e = 83282l;
+        short f = 32767;
+        boolean g = true;
+        byte h = 120;
+        // int aman;
+
+        System.out.println("The int variable is " + a);
+        System.out.println("The float variable is " + b);
+        System.out.println("The double variable is " + c);
+        System.out.println("The char variable is " + d);
+        System.out.println("The long variable is " + e);
+        System.out.println("The short variable is " + f);
+        System.out.println("The boolean variable is " + g);
+        System.out.println("The byte variable is " + h);
+        // System.out.println(aman); // java compiler doesn't initialize an
+        // uninitialized variable with a value
+
+        // Non Primitive Data Types
+        /*
+         * String
+         * Array
+         * Classes
+         * Interfaces
+         */
+
+    }
+}
+/*
+ * OUTPUT
+ * 
+ * The int variable is 78
+ * The float variable is 32.22
+ * The double variable is 232.22343
+ * The char variable is f
+ * The long variable is 83282
+ * The short variable is 32767
+ * The boolean variable is true
+ * The byte variable is 120
+ */
